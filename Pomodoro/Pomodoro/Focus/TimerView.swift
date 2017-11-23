620aff77473d7e669a88b1076b792b7937f925ab
CaoLu <leon_cao@kingdee.com>
2017-11-23 18:28:56 +0800

    finish UI

--- Pomodoro/Pomodoro/Focus/TimerView.swift
+++ Pomodoro/Pomodoro/Focus/TimerView.swift
@@ -66,6 +66,47 @@ class TimerView: UIView {
         settingFullRingStyle()
     }
     
+    func updateTimer() {
+        
+        var percentage: CGFloat
+        var dummyInt: Int
+        if !showRemaining {
+            dummyInt = Int(100000.0*(1 - (durationInSeconds-1) / maxValue))
+            //            percentage = 1 - durationInSeconds / maxValue
+        } else {
+            dummyInt = Int(100000.0*(durationInSeconds-1) / maxValue)
+            //            percentage = durationInSeconds / maxValue
+        }
+        percentage = CGFloat(dummyInt)/100000.0
+        
+        var secondsPercentage: CGFloat
+        if showRemaining {
+            secondsPercentage = (durationInSeconds-1).truncatingRemainder(dividingBy: 60.0)
+        } else {
+            secondsPercentage = 60.0 - (durationInSeconds-1).truncatingRemainder(dividingBy: 60.0)
+            
+        }
+        
+        if durationInSeconds > 0 && !showRemaining {
+            durationInSeconds = maxValue - durationInSeconds
+        }
+        let seconds = Int(durationInSeconds.truncatingRemainder(dividingBy: 60))
+        let minutes = Int(durationInSeconds / 60.0)
+//        let format = "02"
+        let labelText = String(format:"%02x",minutes) + ":" + String(format:"%02x",seconds)
+        
+        timeLabel.text = labelText
+        
+        let totalMinutes = (maxValue-1) / 60
+        let dashLength = 2*radius*CGFloat(Double.pi)/totalMinutes
+        let lineDashPatternFirstLength = (dashLength-CGFloat(2)) as NSNumber  //TODO: Maybe not too beautiful
+        timerShapeLayer.lineDashPattern = [lineDashPatternFirstLength, 2]
+        
+        timerShapeLayer.strokeEnd = percentage
+        secondsShapeLayer.strokeEnd = CGFloat(secondsPercentage) / 60.0
+    }
+    
+    
     
     private func initTimerState() {
         timerShapeLayer.strokeEnd = 0.0
@@ -82,12 +123,28 @@ class TimerView: UIView {
     }
     
     private func settingSecondsRingStyle() {
-        secondsRingPath = UIBezierPath(arcCenter: timerCenter, radius: radius-4, startAngle: startAngle, endAngle: endAngle, clockwise: <#T##Bool#>)
+        secondsRingPath = UIBezierPath(arcCenter: timerCenter, radius: radius-4, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        secondsShapeLayer.path = secondsRingPath.cgPath
+        secondsShapeLayer.fillColor = UIColor.clear.cgColor
+        secondsShapeLayer.strokeColor = TimeStyleKit.timerColor.cgColor
+        secondsShapeLayer.lineWidth = 1.0
     }
     
     private func settingFullRingStyle() {
+        TimeStyleKit.timerColor.set()
         
+        fullRingPath = UIBezierPath(arcCenter: timerCenter, radius: radius+4, startAngle: startAngle, endAngle: endAngle, clockwise: true)
+        
+        fullRingPath.lineWidth = 1.0
+        fullRingPath.stroke()
     }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        showRemaining = !showRemaining
+        updateTimer()
+    }
+    
+    
 }
 
 
